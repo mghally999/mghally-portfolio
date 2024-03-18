c683ad8f2086866b3e760be40b0da501fd58a6c9
mohammedghallydev <mohammed.ghally110@gmail.com>
2024-03-18 16:49:56 +0400

    Updating Home Section

--- pages/layout.js
+++ pages/layout.js
@@ -31,9 +31,8 @@ export default function Layout({ children }) {
       <div className='flex relative h-full justify-between gap-x-3'>
         {/* left most side */}
         <div
-          className={`w-64 h-screen left-0 lg:rounded-xl -top-10 lg:top-0 lg:left-0 lg:h-full overflow-hidden bg-DeepNightBlack shadow-2xl z-50 lg:flex flex-col  lg:relative ${
-            intro ? 'flex absolute' : 'hidden'
-          }`}>
+          className={`w-64 h-screen left-0 lg:rounded-xl -top-10 lg:top-0 lg:left-0 lg:h-full overflow-hidden bg-DeepNightBlack shadow-2xl z-50 lg:flex flex-col  lg:relative ${intro ? 'flex absolute' : 'hidden'
+            }`}>
           <Intro isOpen={intro} setIsOpen={setIntro} />
         </div>
         {/* overlay */}
@@ -52,7 +51,7 @@ export default function Layout({ children }) {
             </span>
           </div>
           <span className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rotate-90 flex items-center justify-center text-center text-xl text-gray-600 font-extrabold tracking-widest'>
-            NavBar
+            {/* NavBar */}
           </span>
         </div>
         {<Nav isOpen={isOpen} setIsOpen={setIsOpen} />}
