86f22ccd8f5cd713fa4be7ef44c92293a48af6a9
mohammedghallydev <mohammed.ghally110@gmail.com>
2024-03-19 18:34:02 +0400

    Resume update

--- components/Common/Intro/Download.jsx
+++ components/Common/Intro/Download.jsx
@@ -3,7 +3,7 @@ const Download = ({ icon }) => {
   return (
     <>
       <Link
-        href="https://drive.google.com/file/d/1190cYj8VgbsRGj50V2My5riaWgP1NMTh/view?usp=sharing"
+        href="https://drive.google.com/file/d/1bP4mNbFl-otAa8hMqRSsXliCl4c7m-Ok/view?usp=sharing"
         target="_blank"
         className="flex flex-row text-LightGray items-center gap-x-4 pb-14 pt-4"
       >
