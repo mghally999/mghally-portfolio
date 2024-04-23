529d4f3370e62affcfc129736aba4e6404495b64
mohammedghallydev <mohammed.ghally110@gmail.com>
2024-04-23 22:01:05 +0400

    Solving Picture Error

--- components/Common/Intro/index.jsx
+++ components/Common/Intro/index.jsx
@@ -8,7 +8,7 @@ import Tools from "./Tools";
 import Skills from "./Skills";
 import Image from "next/image";
 import { NAME, DESIGNATION, SOCIAL_LINKS } from "../../../constants/constants";
-import Conan from "../../../public/images/Mohammed Ghally.png";
+import Conan from "../../../public/images/Ghally.png";
 
 const Intro = () => {
   return (
