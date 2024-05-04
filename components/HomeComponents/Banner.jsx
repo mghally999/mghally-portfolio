abd4494edff5ef6669a27fc058bbb9c5409150b5
mohammedghallydev <mohammed.ghally110@gmail.com>
2024-05-04 15:50:07 +0400

    Updating Experience

--- components/HomeComponents/Banner.jsx
+++ components/HomeComponents/Banner.jsx
@@ -65,7 +65,7 @@ const Banner = () => {
         <div className="grid grid-cols-2 gap-4 md:gap-0 md:flex items-center justify-between w-full px-4 xl:px-8 2xl:px-16">
           <div className="flex items-center gap-x-1">
             <span className="text-base md:text-lg text-Green font-bold">
-              2+
+              4+
             </span>
             <span className="text-xs text-Snow">Years Experience</span>
           </div>
